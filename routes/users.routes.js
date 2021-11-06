461778eb70b76ac3169ebe45e57752906c7e6f04
lewleal <lewleal2012@gmail.com>
2021-11-06 20:40:31 -0300

    Delete Token Ok

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,6 @@
-let express = require('express');
-const { authJwt } = require("../Middleware/authJwt");
-let router = express.Router();
+const express = require('express');
+const { authJwt } = require("../Middleware");
+const router = express.Router();
  
 const user = require('../controllers/user.controllers');
 router.post('/api/user/login', user.signin);
@@ -8,7 +8,7 @@ router.post('/api/user/create', user.createUser);
 router.get('/api/user/all', user.retrieveAllUser);
 router.get('/api/user/onebyid/:id', user.getUserById);
 router.put('/api/user/update/:id', user.updateById);
-// router.delete('/api/user/:id', authJwt.verifyToken, user.deleteById);
+router.delete('/api/user/:id', authJwt.verifyToken, user.deleteById);
 
 
 module.exports = router;
